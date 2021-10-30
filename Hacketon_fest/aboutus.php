cfb3be493e8d992ef53679a73f759debd4c1e946
rupasri209 <rupa209.ravuri@gmail.com>
2021-10-30 11:38:59 +0530

    final code

---
+++ Hacketon_fest/aboutus.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+ <link rel="stylesheet" type="text/css" href="styleabout.css">
+</head>
+<body>
+<div class="about-section">
+  <div class="inner-container">
+    <h1>About Us</h1>
+    <p class="text">CookBook features the largest collection of professionally-tested recipes online from food experts at the magazines and cookbooks you love and trust--including Cooking Light, Southern Living, Sunset, Coastal Living, Real Simple, and more.
+</p>
+<h><b>SEARCH</b></h>
+<p>Find the food you need for any occasion with our recipe finder. Easily refine your search with any of nearly 150 recipe categories. The site delivers every type of recipe you could want, from decadent desserts and authentic cuisines to great-tasting healthy favorites with nutrition information that is reviewed by registered dietitians.</p>
+<h><b>SAVOR</b></h>
+<p>Enjoy daily features that include quick meal ideas, seasonal menus, cooking videos, cookbook reviews and our take on the latest gadgets. Get holiday tips and entertaining ideas for any size gathering. You'll also find thousands of big, mouthwatering photos and scores of how-to videos with tips to help you cook with confidence.</p>
+<h><b>SHARE</b></h>
+<p>Save your favorite recipes. Build and email your shopping lists. Create and pass around menus for upcoming get-togethers. Rate the recipes and discover new must-try dishes from thousands of reviews from fellow cooks.</p>
+<h><b>COME BACK FOR SECONDS</b></h>
+<p>This is just the first course! In the coming months we'll be beefing up search, uploading even more gorgeous photos, and expanding personalized features to make MyRecipes.com the best food site online. Sign up for our weekly newsletter to get content updates and notification of site improvements.</p>
+
+</body>
+</html>
